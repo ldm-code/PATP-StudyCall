7b023e63682d66295f9c71122b1c176a0277924e
Leonardo Moraes <demoraesleonardo327@gmail.com>
2025-09-04 16:43:57 -0300

    Create instituicao_controle.py

---
+++ controller/instituicao_controle.py
@@ -0,0 +1,24 @@
+# controller/instituicao_controller.py
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))  
+from model.instituicao import instituicao
+import hashlib
+
+class InstituicaoController:
+
+    @staticmethod
+    def criar_instituicao(nome, cnpj, senha, email, matricula):
+
+        # --- Validação básica ---
+        if not nome.strip() or not cnpj.strip() or not senha.strip() or not email.strip():
+            raise ValueError("Todos os campos devem ser preenchidos")
+        try:
+            matricula = int(matricula)
+        except ValueError:
+            raise ValueError("Matrícula deve ser um número")
+        
+
+        facul = instituicao(nome, cnpj, senha, email, matricula)
+
+        return facul
