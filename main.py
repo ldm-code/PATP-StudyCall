0f5fdbf00b606baf1a2b24f27216ae1ecc0d7fd6
Leonardo Moraes <demoraesleonardo327@gmail.com>
2025-09-25 21:25:00 -0300

    commit

--- main.py
+++ main.py
@@ -1,34 +1,7 @@
-from PyQt5 import QtWidgets
-import mysql.connector
-from view.interface_inicial import Ui_Form as UiInicial
-from view.interface_user import Ui_Form as UiUser
-from view.interface_instituicao import Ui_Form as UiInstituicao
-class MainApp(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        self.tela_inicial = UiInicial()
-        self.tela_inicial.setupUi(self)
-        self.tela_inicial.usuario.clicked.connect(self.abrir_tela_usuario)
-        self.tela_inicial.instituicao.clicked.connect(self.abrir_tela_facul)
-    def abrir_tela_usuario(self):
-        self.tela_usuario = QtWidgets.QWidget()
-        self.ui_usuario = UiUser()
-        self.ui_usuario.setupUi(self.tela_usuario)
-        self.tela_usuario.show()
-        self.close()
-    def abrir_tela_facul(self):
-        self.tela_instituicao=QtWidgets.QWidget()
-        self.ui_instituicao=UiInstituicao()
-        self.ui_instituicao.setupUi(self.tela_instituicao)
-        self.tela_instituicao.show()
-        self.close()
-
-
-
-if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    main_window = MainApp()
-    main_window.show()
-    sys.exit(app.exec_())
-
+from model.usuario import Usuario
+nome=input("seu nome:")
+email=input("seu email:")
+senha=input('sua senha:')
+professor=input("seu tipo:")
+user=Usuario(nome,email,senha,professor)
+user.salvar()
--- model/usuario.py
+++ model/usuario.py
@@ -1,8 +1,30 @@
 import mysql.connector
+
 class Usuario:
-          def __init__(self,nome,email,senha,tipo):
-                  self.nome=nome
-                  self.email=email
-                  self.senha=senha
-                  self.tipo=tipo   
- 
+    def __init__(self, nome, email, senha, tipo):
+        self.nome = nome
+        self.email = email
+        self.senha = senha
+        self.tipo = tipo   
+    
+    def salvar(self):          
+        try:
+            conexao = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="root",
+                database="patp"
+            )
+            cursor = conexao.cursor()
+            sql = "INSERT INTO usuario (nome, email, senha, tipo) VALUES (%s, %s, %s, %s)"
+            valores = (self.nome, self.email, self.senha, self.tipo)
+            cursor.execute(sql, valores)
+            conexao.commit()
+            print("Usuário salvo com sucesso!")
+        except mysql.connector.Error as erro:
+            print(f" Erro ao salvar usuário: {erro}")
+        finally:
+            if cursor:
+                cursor.close()
+            if conexao:
+                conexao.close()
