2cefbd77190d90df1ca97732cc6b21455eb89bc2
Leonardo Moraes <demoraesleonardo327@gmail.com>
2025-10-13 21:25:11 -0300

    explicacao do crud

--- model/instituicao.py
+++ model/instituicao.py
@@ -1,5 +1,5 @@
 import mysql.connector
-from model.crud_banco import salvar_banco
+from model.crud_banco import salvar_banco #chamando a funcao no codigo
 class Instituicao:
           def __init__(self,nome,senha,matricula,cnpj,email):
                   self.nome=nome
@@ -7,18 +7,19 @@ class Instituicao:
                   self.senha=senha
                   self.email=email
                   self.matricula=matricula
-          def salvar(self):
-              try:
-                  conexao=salvar_banco()
-                  cursor=conexao.cursor()
+          def salvar(self): # modulo que salva no banco
+              try: # try e exept para tratar erros do banco,nada diretamente ligado ao curd
+                  conexao=salvar_banco() #criando a conexao com a funcao que criamos
+                  cursor=conexao.cursor() #criando o cursor para executar o comanco
                   comando="INSERT INTO instituicao(nome,senha,matricula,CNPJ,email) VALUES(%s,%s,%s,%s,%s)"
-                  dados=( self.nome ,self.senha,self.matricula,self.cnpj,self.email)
-                  cursor.execute(comando,dados)
-                  conexao.commit()
+                  # variavel comando para executar a querry (comando mysql)
+                  dados=( self.nome ,self.senha,self.matricula,self.cnpj,self.email) # passando os dados da classe
+                  cursor.execute(comando,dados) #executando a querry para enviar dados ao banco
+                  conexao.commit()# comando que finaliza a querry
                   print("salvou os dados com sucesso")
-              except mysql.connector.Error as e:
+              except mysql.connector.Error as e:#tratamento de erros
                      print(f"erro !,{e}")
-              finally:
+              finally:# fechamento do cursor e da conexao
                      if cursor:
                             cursor.close()
                      if conexao:
