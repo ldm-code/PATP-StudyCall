ebeb2ef1cf68cfd8ed597c3c1d15cb3d8b7adcd0
Leonardo Moraes <demoraesleonardo327@gmail.com>
2025-10-20 20:05:06 -0300

    update

--- main.py
+++ main.py
@@ -2,7 +2,31 @@ from model.usuario import Usuario
 from model.instituicao import Instituicao
 from model.chamado import Chamado
 
+from PyQt5 import QtWidgets
+import sys
+from view.user import Ui_Dialog  # importa a tela do usuário
+
+def main():
+    # Cria a aplicação Qt
+    app = QtWidgets.QApplication(sys.argv)
+
+    # Cria a janela de diálogo (base da interface)
+    janela = QtWidgets.QDialog()
+
+    # Cria o objeto da tela gerada pelo Qt Designer
+    tela = Ui_Dialog()
+
+    # Configura os elementos da interface na janela
+    tela.setupUi(janela)
+
+    # Exibe a janela na tela
+    janela.show()
+
+    # Mantém o programa rodando até o usuário fechar
+    sys.exit(app.exec_())
 
 
+if __name__ == "__main__":
+    main()
 
 
