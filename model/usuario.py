e19658da4708b79175327284f7298d255b132f1b
Leonardo Moraes <demoraesleonardo327@gmail.com>
2025-10-27 19:39:30 -0300

    update

--- model/usuario.py
+++ model/usuario.py
@@ -9,33 +9,20 @@ class Usuario:
         self.senha = senha
         self.tipo = tipo   
 
-    def salvar(self): 
-         
+    def salvar(self):         
         try:
-            
-            conexao = banco()
-           
-            cursor = conexao.cursor()
-           
+            conexao = banco()          
+            cursor = conexao.cursor()           
             sql = "INSERT INTO usuario (nome, email, senha, tipo) VALUES (%s, %s, %s, %s)"
-            valores = (self.nome, self.email, self.senha, self.tipo)
-          
-
-            cursor.execute(sql, valores)
-            
-
+            valores = (self.nome, self.email, self.senha, self.tipo)          
+            cursor.execute(sql, valores)           
             conexao.commit()
-           
-
             return cursor.lastrowid
         except mysql.connector.Error as erro:
             print(f" Erro ao salvar usuário: {erro}")
         finally:
-            if cursor:
-                
-                cursor.close()
-               
-            if conexao:
-          
+            if cursor:               
+                cursor.close()              
+            if conexao:          
                 conexao.close()
                      
